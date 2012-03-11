f4b2a562d5f4d813fc5a942b2694de0e36abcc0a
Alban Peignier <alban@tryphon.eu>
2012-03-11 14:59:05 +0100

    Add languages support

--- lib/user_interface/engine.rb
+++ lib/user_interface/engine.rb
@@ -1,4 +1,12 @@
 module UserInterface
   class Engine < Rails::Engine
+
+    initializer 'user_interface.app_controller' do |app|
+      ActiveSupport.on_load(:action_controller) do
+        require 'user_interface/language_support'
+        include UserInterface::LanguageSupport
+      end
+    end
+
   end
 end

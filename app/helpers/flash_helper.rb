57afc7b7a6f003fd47e021ff2f27c6af1ecdb803
Luc Donnet <luc.donnet@dryade.net>
2012-03-19 13:39:17 +0100

    Fix flash helper to use html_safe

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,13 +1,9 @@
 module FlashHelper
 
   def flash_tag(flash)
-    unless flash.empty?
-      content_tag(:div, :id => "flash") do
-        flash.collect do |level, message|
-          content_tag :div, sanitize(message), :class => level
-        end.join("\n")
-      end
-    end
+    flash.collect do |level, message|
+      content_tag(:div, sanitize(message), :class => level)
+    end.join("\n").html_safe
   end
 
 end

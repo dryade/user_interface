190017865daecbef03441c1227fe93b15e1c6b8c
Alban Peignier <alban@tryphon.eu>
2012-06-27 17:23:31 +0200

    Capitalize tab_link_to name

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -1,7 +1,7 @@
 module TabsHelper
 
   def tab_link_to(model_or_name, link, current=nil)
-    model_or_name = model_or_name.model_name.human.pluralize if Class === model_or_name
+    model_or_name = model_or_name.model_name.human.capitalize.pluralize if Class === model_or_name
     current ||= request.path.start_with?(link)
     link_to model_or_name, link, :class => ("current" if current)
   end
